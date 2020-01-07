6bcfc389dbe8b38dcdc2c92189d85a7e64d50208
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2020-01-07 20:32:58 +0100

    first commit

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -4,7 +4,7 @@ const Person = ({link,image,isBigGirl,isRightGirl}) => {
     return(
         <div className={"chica " + (isBigGirl ? 'chica-grande ' : '')+ (isRightGirl ? 'grande-derecha' : '')}>
             <a className="link" href={link} title="">
-                {/*<span className="thumb"><img src={image} width="175" height="150" alt="" title=""/></span>*/}
+                <span className="thumb"><img src={image} width="175" height="150" alt="" title=""/></span>
                 <span className="nombre-chica"> <span className="ico-online"></span> Nombre de la chica</span>
                 <span id="favorito" className="ico-favorito"></span>
             </a>
